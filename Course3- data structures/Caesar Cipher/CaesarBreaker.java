7236c2b60240141f650299719ab1da60b8b835f9
anamendes23 <anamendes23@gmail.com>
2020-07-15 19:41:30 -0700

    organized folders

---
+++ Course3- data structures/Caesar Cipher/CaesarBreaker.java
@@ -0,0 +1,14 @@
+/* The CaesarBreaker class implements an algorithm to decrypt a message encrypted using
+ * Caesar Cipher encryption method without knowing the key.
+ * The logic is to rely on letter frequencies in English.
+ * In this case, we assumed that 'e' is the most frequent letter. Therefore, the 
+ * decryption method will look for the most frequent letter and assume it is 'e',
+ * to then calculate the key and attempt to decryp the message.
+ */
+
+public class CaesarBreaker {
+
+
+
+
+}
